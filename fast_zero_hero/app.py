f4785cf6d65446cfec94d7b1211c6853de1b81e3
Lyarkh <lcemanuel.emanuel@gmail.com>
2024-07-13 18:39:20 -0300

    feat: adding the first response model in schemas

--- fast_zero_hero/app.py
+++ fast_zero_hero/app.py
@@ -1,8 +1,12 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+
+from schemas import Message
 
 app = FastAPI()
 
 
-@app.get('/')
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Hello world'}

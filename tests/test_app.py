9884c36d0f12f0ed83b8cb8960d4b5fbde319abd
Lyarkh <lcemanuel.emanuel@gmail.com>
2024-07-23 22:53:46 -0300

    test: adding some tests to hello world in HTML

--- tests/test_app.py
+++ tests/test_app.py
@@ -12,3 +12,10 @@ def test_read_root_deve_retornar_ok_e_ola_mundo():
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'Hello world'}
+
+
+def test_read_root_html_deve_retornar_ok_e_ola_mundo_com_html_h1():
+    response = client.get('/hello-world-html')
+
+    assert response.status_code == HTTPStatus.OK
+    assert '<h1>Hello world</h1>' in response.text

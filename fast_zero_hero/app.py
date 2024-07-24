d0a8bf353dabc35b4092e12bbe2f4e0f06b26892
Lyarkh <lcemanuel.emanuel@gmail.com>
2024-07-23 22:53:25 -0300

    feat: adding the exercise of hello world in HTMLResponse

--- fast_zero_hero/app.py
+++ fast_zero_hero/app.py
@@ -1,6 +1,8 @@
 from http import HTTPStatus
 
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
 from fast_zero_hero.schemas import Message
 
 app = FastAPI()
@@ -9,3 +11,8 @@ app = FastAPI()
 @app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Hello world'}
+
+
+@app.get('/hello-world-html', status_code=HTTPStatus.OK)
+def read_root_html():
+    return HTMLResponse('<h1>Hello world</h1>')

b10500b514c35dd2f64a373fde1eb79b3dce6b1f
Lyarkh <lcemanuel.emanuel@gmail.com>
2024-09-28 17:57:16 -0300

    test: creating fixture for client

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,29 +1,30 @@
 from http import HTTPStatus
 
+import pytest
 from fastapi.testclient import TestClient
 
 from fast_zero_hero.app import app
 
-client = TestClient(app)
 
+@pytest.fixture()
+def client():# -> Callable[[], ...]:
+    return TestClient(app)
 
-def test_read_root_deve_retornar_ok_e_ola_mundo():
+def test_read_root_deve_retornar_ok_e_ola_mundo(client):
     response = client.get('/')
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'Hello world'}
 
 
-def test_read_root_html_deve_retornar_ok_e_ola_mundo_com_html_h1():
+def test_read_root_html_deve_retornar_ok_e_ola_mundo_com_html_h1(client):
     response = client.get('/hello-world-html')
 
     assert response.status_code == HTTPStatus.OK
     assert '<h1>Hello world</h1>' in response.text
 
 
-def test_create_user():
-    client = TestClient(app)
-
+def test_create_user(client):
     response = client.post(
         '/users/',
         json={
